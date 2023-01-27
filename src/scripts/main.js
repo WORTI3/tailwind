fa822e352c1135ee7701c11636672bd8a4271c25
Atech <[mc8atech@gmail.com]>
2023-01-27 15:46:47 +0000

    chore(scripts) added progress bar and nav chages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 import '../styles/main.css';
+
+// Source: https://alligator.io/js/progress-bar-javascript-css-variables/
+document.addEventListener('scroll', function() {
+    var docElement = document.documentElement,
+    body = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight',
+    progress = document.querySelector('#progress'),
+    scroll;
+    var scrollpos = window.scrollY;
+    var header = document.getElementById("header");
+    var navcontent = document.getElementById("nav-content");
+    /*Refresh scroll % width*/
+    scroll = (docElement[st] || body[st]) / ((docElement[sh] || body[sh]) - docElement.clientHeight) * 100;
+    progress.style.setProperty('--scroll', scroll + '%');
+
+    /*Apply classes for slide in bar*/
+    scrollpos = window.scrollY;
+    const background = "bg-white-smoke";
+
+    if (scrollpos > 10) {
+        header.classList.add(background);
+        header.classList.add("shadow");
+        navcontent.classList.add(background);
+        navcontent.classList.remove("bg-transparent");
+    } else {
+        navcontent.classList.add("bg-transparent");
+        header.classList.remove(background);
+        header.classList.remove("shadow");
+        navcontent.classList.remove(background);
+    }
+});
+
+document.getElementById('nav-toggle').onclick = function() {
+    document.getElementById("nav-content").classList.toggle("hidden");
+};
