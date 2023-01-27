d8e0f267f11595fe9eba5a297ffb898a8a4e6fcd
Atech <[mc8atech@gmail.com]>
2023-01-27 15:36:04 +0000

    chore(config) markdown render support and output url update

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,29 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const markdownIt = require("markdown-it");
 
+module.exports = function (eleventyConfig) {
+  let mdOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
 
-module.exports = function(eleventyConfig) {
+  eleventyConfig.setLibrary("md", markdownIt(mdOptions));
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy("src/img");
 
+  const { DateTime } = require("luxon");
 
-  const {
-    DateTime
-  } = require("luxon");
-
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).setLocale('en-gb').toLocaleString(DateTime.DATE_HUGE);
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj)
+      .setLocale("en-gb")
+      .toLocaleString(DateTime.DATE_HUGE);
   });
 
   return {
-    dir: { input: 'src', output: '_site' }
+    dir: { input: "src", output: "_site/wortie" },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
   };
 };
