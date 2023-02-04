1260c413b074552156d2762a73976f679f40108f
Archie <archie.wort@nhs.net>
2023-02-04 14:36:36 +0000

    chore(scripts) updated js for hamburg menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,40 @@
-import '../styles/main.css';
+import "../styles/main.css";
 
 // Source: https://alligator.io/js/progress-bar-javascript-css-variables/
-document.addEventListener('scroll', function() {
-    var docElement = document.documentElement,
+document.addEventListener("scroll", function () {
+  var docElement = document.documentElement,
     body = document.body,
-    st = 'scrollTop',
-    sh = 'scrollHeight',
-    progress = document.querySelector('#progress'),
+    st = "scrollTop",
+    sh = "scrollHeight",
+    progress = document.querySelector("#progress"),
     scroll;
-    var scrollpos = window.scrollY;
-    var header = document.getElementById("header");
-    var navcontent = document.getElementById("nav-content");
-    /*Refresh scroll % width*/
-    scroll = (docElement[st] || body[st]) / ((docElement[sh] || body[sh]) - docElement.clientHeight) * 100;
-    progress.style.setProperty('--scroll', scroll + '%');
+  var scrollpos = window.scrollY;
+  var header = document.getElementById("header");
+  var navcontent = document.getElementById("nav-content");
+  /*Refresh scroll % width*/
+  scroll =
+    ((docElement[st] || body[st]) /
+      ((docElement[sh] || body[sh]) - docElement.clientHeight)) *
+    100;
+  progress.style.setProperty("--scroll", scroll + "%");
 
-    /*Apply classes for slide in bar*/
-    scrollpos = window.scrollY;
-    const background = "bg-white-smoke";
+  /*Apply classes for slide in bar*/
+  scrollpos = window.scrollY;
+  const background = "bg-white-smoke";
 
-    if (scrollpos > 10) {
-        header.classList.add(background);
-        header.classList.add("shadow");
-        navcontent.classList.add(background);
-        navcontent.classList.remove("bg-transparent");
-    } else {
-        navcontent.classList.add("bg-transparent");
-        header.classList.remove(background);
-        header.classList.remove("shadow");
-        navcontent.classList.remove(background);
-    }
+  if (scrollpos > 10) {
+    header.classList.add(background);
+    header.classList.add("shadow");
+    navcontent.classList.add(background);
+    navcontent.classList.remove("bg-transparent");
+  } else {
+    navcontent.classList.add("bg-transparent");
+    header.classList.remove(background);
+    header.classList.remove("shadow");
+    navcontent.classList.remove(background);
+  }
 });
 
-document.getElementById('nav-toggle').onclick = function() {
-    document.getElementById("nav-content").classList.toggle("hidden");
-};
+document.addEventListener("click", function () {
+  document.getElementById("nav-content").classList.toggle("hidden");
+});
