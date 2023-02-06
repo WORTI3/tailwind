0eae268cc20bfdbf38fb40f5829323d41614c097
Archie <archie.wort@nhs.net>
2023-02-06 22:34:21 +0000

    chore(components) blog navigation functionality

--- .eleventy.js
+++ .eleventy.js
@@ -21,12 +21,56 @@ module.exports = function (eleventyConfig) {
       .setLocale("en-gb")
       .toLocaleString(DateTime.DATE_HUGE);
   });
+  eleventyConfig.addFilter("setAttribute", function (dictionary, key, value) {
+    dictionary[key] = value;
+    return dictionary;
+  });
+
+  eleventyConfig.addCollection("categories", (collectionApi) => {
+    const posts = collectionApi
+      .getFilteredByTag("posts")
+      .filter((p) => !p.data.tags.includes("draft"));
+
+    return posts.reduce(
+      (tags, post) => {
+        post.data.blogTags
+          .filter((tag) => tag !== "posts")
+          .forEach((tag) => {
+            if (!tags[tag.title]) {
+              tags[tag.title] = 0;
+            }
+            tags[tag.title]++;
+          });
+        return tags;
+      },
+      { "All posts": posts.length }
+    );
+  });
+
+  eleventyConfig.addFilter("categoryFilter", function (collection, category) {
+    console.log("debug cat:" + category);
+    if (!category) return collection;
+    console.log(category);
+    const filtered = collection.filter((p) => p._data.blogTags.includes(category));
+    // const filtered = collection.filter(
+    //   (tags, post) => post.data.blogTags.title === category
+    // );
+    return filtered;
+  });
+
+  eleventyConfig.addFilter("pluck", function (arr, value) {
+    if (!value) return collection;
+    var filtered = arr.filter((p) => p.data.blogTags["title"] == value);
+    console.log("filtered: " + filtered);
+    // console.log("reduced: " + reduced);
+    return filtered;
+  });
 
   return {
-    dir: { 
+    dir: {
       input: "src",
       includes: "_includes",
-      output: "_site/wortie" 
+      output: "_site/wortie",
     },
     templateFormats: ["njk", "md"],
     dataTemplateEngine: "njk",
