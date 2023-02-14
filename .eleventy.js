e9973ec425b8404583c7a2ecd8fbaa265ebb825c
wortie <archiewort@icloud.com>
2023-02-14 14:49:37 +0000

    chore(develop) PR for latest dev changes (#2)
    📔

--- .eleventy.js
+++ .eleventy.js
@@ -26,46 +26,6 @@ module.exports = function (eleventyConfig) {
     return dictionary;
   });
 
-  eleventyConfig.addCollection("categories", (collectionApi) => {
-    const posts = collectionApi
-      .getFilteredByTag("posts")
-      .filter((p) => !p.data.tags.includes("draft"));
-
-    return posts.reduce(
-      (tags, post) => {
-        post.data.blogTags
-          .filter((tag) => tag !== "posts")
-          .forEach((tag) => {
-            if (!tags[tag.title]) {
-              tags[tag.title] = 0;
-            }
-            tags[tag.title]++;
-          });
-        return tags;
-      },
-      { "All posts": posts.length }
-    );
-  });
-
-  eleventyConfig.addFilter("categoryFilter", function (collection, category) {
-    console.log("debug cat:" + category);
-    if (!category) return collection;
-    console.log(category);
-    const filtered = collection.filter((p) => p._data.blogTags.includes(category));
-    // const filtered = collection.filter(
-    //   (tags, post) => post.data.blogTags.title === category
-    // );
-    return filtered;
-  });
-
-  eleventyConfig.addFilter("pluck", function (arr, value) {
-    if (!value) return collection;
-    var filtered = arr.filter((p) => p.data.blogTags["title"] == value);
-    console.log("filtered: " + filtered);
-    // console.log("reduced: " + reduced);
-    return filtered;
-  });
-
   return {
     dir: {
       input: "src",
